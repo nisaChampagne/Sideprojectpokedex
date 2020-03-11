a6a3caa0fa66a18462ab321d9803e561f013cd2a
Nisa Champagne <nchampag23@gmail.com>
2020-03-11 17:43:24 -0400

    mobile view better

--- pokedex/src/components/PokeScreen.js
+++ pokedex/src/components/PokeScreen.js
@@ -5,8 +5,8 @@ import { Card } from "semantic-ui-react"
 export default function PokeScreen(props) {
   return (
     <Card className='ui centered class'>
-      <Card.Content className='screen'>
-        <Card.Header> {props.name}</Card.Header>
+      <Card.Content className='screen' style={{margin: '0 auto'}}>
+        <Card.Header > {props.name}</Card.Header>
       </Card.Content>
     </Card>
   );
